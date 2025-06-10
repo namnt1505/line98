1d52691fbcb897ca4fcd2d0c96ad64dc6e643baf
Lucas Ng. <namnt1505@gmail.com>
2025-06-10 01:40:30 +0000

    test workflow

--- components/game/game-over-message.tsx
+++ components/game/game-over-message.tsx
@@ -1,3 +1,3 @@
 export default function GameOverMessage() {
-  return <div className="mt-4 text-center font-bold text-red-500">Game Over! No more moves available. Restart Now!</div>
+  return <div className="mt-4 text-center font-bold text-red-500">Game Over! No more moves available.</div>
 }
